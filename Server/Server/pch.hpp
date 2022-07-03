456f49978b28efc7e6026aeb60a8cb67e7bcd0f6
Iconer <yoyofa2@hotmail.com>
2022-07-04 04:42:12 +0900

    프로토콜 열거형 선언

--- Server/Server/pch.hpp
+++ Server/Server/pch.hpp
@@ -7,6 +7,7 @@
 class Framework;
 class Session;
 class PlayingSession;
+class Room;
 
 class Asynchron;
 class Packet;
@@ -33,11 +34,24 @@ constexpr unsigned int MAX_NPCS = MAX_ROOMS * MAX_NPCS_PER_ROOM;
 constexpr unsigned int NPC_ID_BEGIN = MAX_USERS;
 
 // 최대 전체 엔티티의 수
-constexpr unsigned int ENTITIES_MAX = MAX_USERS + MAX_NPCS;
+constexpr unsigned int MAX_ENTITIES = MAX_USERS + MAX_NPCS;
 
 // 최대 매치의 수
 constexpr unsigned int MAX_SEARCHS = 500;
 
 extern unsigned int numberRooms = 0;
+
+enum class Protocol : unsigned int
+{
+	CS_SIGNIN = 0, // 로그인
+	CS_SIGNUP, // 계정 가입
+	CS_SIGNOUT, // 로그아웃
+
+
+
+	SC_SIGNIN_SUCCESS = 1000, // 로그인 성공 알림
+	SC_SIGNIN_FAILURE, // 로그인 실패 알림
+
+};
 
 #endif // ! __PCH__
