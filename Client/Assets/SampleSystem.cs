5bfd7aed2df967ad39c8dfb6e782510ad6843b9d
Iconer <yoyofa2@hotmail.com>
2022-07-18 01:42:58 +0900

    샘플 클라이언트에 프로토콜 열거형 추가

--- Client/Assets/SampleSystem.cs
+++ Client/Assets/SampleSystem.cs
@@ -2,22 +2,68 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using System;
 using System.Net.Sockets;
+using System.Net;
+using System.Net.Http;
 
 public class SampleSystem : MonoBehaviour
 {
-    public TcpClient tcpClient;
-    public UdpClient udpClient;
+	public IPEndPoint serverAddress;
+	public TcpClient tcpClient;
+	public UdpClient udpClient;
+	public NetworkStream tcpStream;
+	public NetworkStream udpStream;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	const int recvFullsize = 1024;
+	public byte[] recvBuffer = new byte[recvFullsize];
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	void Awake()
+	{
+		serverAddress = new(IPAddress.Loopback, 9000);
+	}
+
+	void Start()
+	{
+		try
+		{
+			tcpClient.Connect(serverAddress);
+		}
+		catch (Exception e)
+		{
+			Debug.LogError(e.Message);
+			tcpClient.Close();
+		}
+
+		tcpStream = tcpClient.GetStream();
+		if (tcpStream is null)
+		{
+			throw new Exception("소켓 연결 실패!");
+		}
+
+		tcpStream.BeginRead(recvBuffer, 0, recvBuffer.Length, CallbackRead, null);
+	}
+
+	void Update()
+	{
+		//if (tcpClient != null && tcpClient.Connected)
+		{ }
+	}
+
+	void CallbackRead(IAsyncResult result)
+	{
+		var byte_recv = tcpStream.EndRead(result);
+
+		//if (result.IsCompleted)
+		if (0 < byte_recv)
+		{
+
+		}
+		else
+		{
+
+		}
+
+		tcpStream.EndRead(result);
+	}
 }
