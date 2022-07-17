9bab1e0f1434e99085a25be9863dde4563ff56a0
Iconer <yoyofa2@hotmail.com>
2022-07-18 00:18:17 +0900

    샘플 클라이언트 스크립트

---
+++ Client/Assets/SampleSystem.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Net.Sockets;
+
+public class SampleSystem : MonoBehaviour
+{
+    public TcpClient tcpClient;
+    public UdpClient udpClient;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
