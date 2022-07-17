41f487bd2872bd5b279f6c66378bd1937b5c59a2
Iconer <yoyofa2@hotmail.com>
2022-07-18 05:18:44 +0900

    클라이언트에서 연결

--- Client/Assets/SampleSystem.cs
+++ Client/Assets/SampleSystem.cs
@@ -15,16 +15,31 @@ public class SampleSystem : MonoBehaviour
 	public NetworkStream tcpStream;
 	public NetworkStream udpStream;
 
-	const int recvFullsize = 1024;
-	public byte[] recvBuffer = new byte[recvFullsize];
+	public int tcpReceived = 0;
+
+	public const int BUFFSIZE = 1024;
+	public byte[] recvBuffer = new byte[BUFFSIZE];
 
 	void Awake()
 	{
-		serverAddress = new(IPAddress.Loopback, 9000);
 	}
 
 	void Start()
 	{
+		Connect();
+	}
+
+	void Update()
+	{
+		//if (tcpClient != null && tcpClient.Connected)
+		{ }
+	}
+
+	void Connect()
+	{
+		serverAddress = new(IPAddress.Loopback, 9000);
+		tcpClient = new(serverAddress);
+
 		try
 		{
 			tcpClient.Connect(serverAddress);
@@ -41,29 +56,89 @@ public class SampleSystem : MonoBehaviour
 			throw new Exception("소켓 연결 실패!");
 		}
 
-		tcpStream.BeginRead(recvBuffer, 0, recvBuffer.Length, CallbackRead, null);
+		RecvTCP(0, BUFFSIZE);
 	}
-
-	void Update()
-	{
-		//if (tcpClient != null && tcpClient.Connected)
-		{ }
-	}
-
 	void CallbackRead(IAsyncResult result)
 	{
+		if (!tcpStream.CanRead)
+		{
+			Debug.LogError("TCP 수신 불가능!");
+			return;
+		}
+
 		var byte_recv = tcpStream.EndRead(result);
+		Debug.Log("TCP 소켓으로 서버로부터 " + byte_recv + "바이트를 받음.");
 
 		//if (result.IsCompleted)
 		if (0 < byte_recv)
 		{
+			if (BitConverter.IsLittleEndian)
+			{
+				Array.Reverse(recvBuffer[(tcpReceived)..(tcpReceived + byte_recv)]);
+			}
 
+			tcpReceived += byte_recv;
+
+			if (4 <= tcpReceived)
+			{
+				var temp_type = BitConverter.ToInt32(recvBuffer[0..4]);
+				var packet_type = (NetworkManager.Protocol)(temp_type);
+
+				if (8 <= tcpReceived)
+				{
+					var packet_size = BitConverter.ToInt32(recvBuffer[4..8]);
+
+					if (packet_size <= tcpReceived)
+					{
+						if (BUFFSIZE <= packet_size)
+						{
+							Debug.LogError("이상한 패킷을 받음!");
+						}
+
+						// 패킷 처리
+						Debug.Log("패킷을 받음: " + packet_type.ToString());
+
+						tcpReceived -= packet_size;
+
+						ShiftLeft(recvBuffer, packet_size);
+					}
+					else
+					{
+						var lack = packet_size - tcpReceived;
+						Debug.Log("TCP 소켓으로부터 받은 바이트가 " + lack + "만큼 부족함.");
+					}
+				}
+				else
+				{
+					Debug.Log("TCP 소켓으로부터 받은 바이트가 부족함.");
+				}
+			}
+			else
+			{
+				Debug.Log("TCP 소켓으로부터 받은 바이트가 부족해서 패킷의 종류를 알 수 없음.");
+			}
 		}
 		else
 		{
-
+			Debug.Log("TCP 소켓으로부터 0바이트를 받음.");
 		}
 
-		tcpStream.EndRead(result);
+		RecvTCP(tcpReceived, BUFFSIZE - tcpReceived);
+	}
+	IAsyncResult RecvTCP(int offset, int size)
+	{
+		return tcpStream.BeginRead(recvBuffer, offset, size, CallbackRead, null);
+	}
+	public static void ShiftLeft(byte[] lst, int shifts)
+	{
+		for (int i = shifts; i < lst.Length; i++)
+		{
+			lst[i - shifts] = lst[i];
+		}
+
+		for (int i = lst.Length - shifts; i < lst.Length; i++)
+		{
+			lst[i] = 0;
+		}
 	}
 }
