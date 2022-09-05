a26e1f0ffe221bd77a0548ccc188ab6bfd5f40db
Iconer <yoyofa2@hotmail.com>
2022-09-05 19:05:07 +0900

    DB 서비스 관리용 클래스 추가

---
+++ Server/Server/DatabaseService.cpp
@@ -0,0 +1,10 @@
+#include "pch.hpp"
+#include "DatabaseService.hpp"
+
+// AZURE SQL DB Çì´õ
+#include "mysql_connection.h"
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/prepared_statement.h>
+
+
---
+++ Server/Server/DatabaseService.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+class DatabaseService
+{
+public:
+	DatabaseService(const std::string_view server, const std::string_view db);
+	~DatabaseService();
+
+
+};
