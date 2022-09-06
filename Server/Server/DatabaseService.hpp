6efef43377ae6f829e0816b0050241f31d9f5323
Iconer <yoyofa2@hotmail.com>
2022-09-07 08:08:29 +0900

    비밀번호는 따로 저장

--- Server/Server/DatabaseService.hpp
+++ Server/Server/DatabaseService.hpp
@@ -16,7 +16,7 @@ public:
 	const std::string myServerAddress = "tcp://iconer-2023.database.windows.net:1433";
 	const std::string myDatabase = "SkyRunner-MainServer";
 	const std::string superUsername = "iconerworks@iconer-2023.database.windows.net";
-	const std::string superUsername = "iconerwokrs";
+	const std::string superPassword = "--";
 	const std::string superPassword = "00000000000";
 
 	sql::Driver* myDriver;
