6c13cc2b6148bc0871248d096a44d401803c595f
Iconer <yoyofa2@hotmail.com>
2022-09-16 06:51:39 +0900

    필요없는 스크립트 삭제

--- Server/Server/DatabaseService.hpp
+++ Server/Server/DatabaseService.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <mysql/jdbc.h>
+//#include <azure/core.hpp>
 
 class DatabaseService
 {
@@ -14,10 +15,10 @@ public:
 	sql::Statement CreateStatement(const std::string_view statement);
 
 	const std::string myServerAddress = "tcp://iconer-2023.database.windows.net:1433";
+
 	const std::string myDatabase = "SkyRunner-MainServer";
 	const std::string superUsername = "iconerworks@iconer-2023.database.windows.net";
-	const std::string superPassword = "--";
-	const std::string superPassword = "00000000000";
+	const std::string superPassword = "faaf853217**";
 
 	sql::Driver* myDriver;
 	sql::Connection* myConnection;
