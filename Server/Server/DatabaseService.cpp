d89517b606fc2873b00c3d7bd132dba851940402
Iconer <yoyofa2@hotmail.com>
2022-09-17 03:40:31 +0900

    MySQL Connector 라이브러리 제거

--- Server/Server/DatabaseService.cpp
+++ Server/Server/DatabaseService.cpp
@@ -2,43 +2,14 @@
 #include "DatabaseService.hpp"
 
 DatabaseService::DatabaseService()
-	: myDriver(nullptr), myConnection(nullptr), myQuery(nullptr), myMutableQuery(nullptr)
 {}
 
 DatabaseService::~DatabaseService()
 {
-	if (myDriver)
-	{
-		myDriver->threadEnd();
-	}
-	if (myConnection)
-	{
-		if (!myConnection->isClosed())
-		{
-			myConnection->close();
-		}
-	}
 }
 
 void DatabaseService::Connect()
 {
-	try
-	{
-		myDriver = ::get_driver_instance();
-		myConnection = myDriver->connect(myServerAddress, superUsername, superPassword);
-	}
-	catch (sql::SQLException& e)
-	{
-		std::cerr << "SQL 예외 발생" << e.what() << "\n";
-		return;
-	}
-	catch (std::exception& e)
-	{
-		std::cerr << "예외 발생: " << e.what() << "\n";
-		return;
-	}
-
-	myConnection->setSchema(myDatabase);
 }
 
 void DatabaseService::Disconnect()
