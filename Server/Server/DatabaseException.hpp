4a7c8face648b120008b47d39ddfab1e2432aa19
Iconer <yoyofa2@hotmail.com>
2022-09-30 19:16:20 +0900

    DB 예외 생성자 최적화

--- Server/Server/DatabaseException.hpp
+++ Server/Server/DatabaseException.hpp
@@ -8,8 +8,7 @@ namespace db
 	public:
 		Exception(const char* const what, std::wstring_view id, std::wstring_view qmsg, SQLINTEGER code)
 			: exception(what)
-			, msg{ qmsg.cbegin(), qmsg.cend() }
-			, state{ id }, native(code)
+			, msg{ qmsg }, state{ id }, native(code)
 		{}
 
 		const SQLINTEGER native;
