b3836529a8cc585292fe1f89a4f639025fedf882
Iconer <yoyofa2@hotmail.com>
2022-09-30 17:17:04 +0900

    기본 동기화 문맥 클래스는 Overlapped 구조체로 스스로를 변환할 수 있음

--- Server/Server/BasicContext.hpp
+++ Server/Server/BasicContext.hpp
@@ -30,6 +30,18 @@ namespace srv
 			, myOperation(op)
 		{}
 
+		[[nodiscard]]
+		inline constexpr WSAOVERLAPPED* Flip() noexcept
+		{
+			return static_cast<WSAOVERLAPPED*>(this);
+		}
+
+		[[nodiscard]]
+		inline constexpr const WSAOVERLAPPED* Flip() const noexcept
+		{
+			return static_cast<const WSAOVERLAPPED*>(this);
+		}
+
 		Operations myOperation;
 	};
 }
